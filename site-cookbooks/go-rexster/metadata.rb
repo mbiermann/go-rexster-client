e8a91e159c3bee42083eae8b099151de5673a2c9
Martin Biermann <info@martinbiermann.com>
2014-02-07 14:59:38 +0100

    Fixes description text in metadata of go-rexster site-cookbook for Vagrant

--- site-cookbooks/go-rexster/metadata.rb
+++ site-cookbooks/go-rexster/metadata.rb
@@ -2,7 +2,7 @@ name              "go-rexster"
 maintainer        "Martin Biermann"
 maintainer_email  "info@martinbiermann.com"
 license           "MIT"
-description       "Installs GO and further dependencies for develop and test go-cluster"
+description       "Installs GO, Titan, Rexster and further dependencies for develop and test go-rexster-client"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version           "0.0.1"
 
